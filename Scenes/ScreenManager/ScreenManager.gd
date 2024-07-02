f2ac66e57de2895b70c4570625bf506f550cbb92
Imahilus <imahilus@frozenchaos.net>
2024-07-02 14:04:31 +0100

    ScreenManager - Initial setup; screen scenes can be added as child nodes under their respective wrapper

---
+++ Scenes/ScreenManager/ScreenManager.gd
@@ -0,0 +1,39 @@
+extends Node
+
+
+class_name ScreenManager
+
+
+enum SCREEN { NONE, MAIN_MENU, SETTINGS, WIZARD_SELECT, GAME, CREDITS }
+
+
+var currentScreen:SCREEN = SCREEN.NONE
+var screenToSwitchTo:SCREEN = SCREEN.MAIN_MENU
+
+
+func _process(_delta):
+	if(screenToSwitchTo != SCREEN.NONE):
+		var currentScreenNode:ScreenWrapper = _get_wrapper_node(currentScreen)
+		var screenNodeToSwitchTo:ScreenWrapper = _get_wrapper_node(screenToSwitchTo)
+		if(screenToSwitchTo != currentScreen && currentScreenNode.get_state_to_switch_to() != ScreenWrapper.STATE.CLOSE):
+			currentScreenNode.close()
+		elif(screenToSwitchTo != currentScreen && currentScreenNode.get_current_state() == ScreenWrapper.STATE.CLOSE):
+			currentScreen = screenToSwitchTo
+			screenToSwitchTo = SCREEN.NONE
+			if(screenNodeToSwitchTo != null):
+				screenNodeToSwitchTo.open()
+
+
+func _get_wrapper_node(screenToGetNodeFor:SCREEN) -> ScreenWrapper:
+	match screenToGetNodeFor:
+		SCREEN.MAIN_MENU:
+			return $MainMenuWrapper
+		SCREEN.SETTINGS:
+			return $SettingsWrapper
+		SCREEN.WIZARD_SELECT:
+			return $WizardSelectWrapper
+		SCREEN.GAME:
+			return $GameWrapper
+		SCREEN.CREDITS:
+			return $CreditsWrapper
+	return null
